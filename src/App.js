b69f455ad2c237ec9516d676927481e3253f9260
Fernando Jose <fernando.jose@luizalabs.com>
2025-04-30 18:40:20 -0300

    refactor: melhorias no template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Reset } from 'styled-reset';
-
-import Home from './pages/Home';
-import theme from './theme';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Reset />
-      <Home />
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
