7aa0fde449cee7ec73c3ad3300c5c779bbca3606
Fernando Jose <fernando.jose@luizalabs.com>
2025-05-04 23:13:40 -0300

    refactor: removendo falso info

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
